3f0898e6106163634f225d1fa1d67151f676bfca
lucastheldl <lucastheldl@gmail.com>
2023-04-08 16:08:49 -0300

    created photoInsertValidation middleware

--- backend/routes/PhotoRoutes.js
+++ backend/routes/PhotoRoutes.js
@@ -4,6 +4,9 @@ const router = express.Router();
 //Controllers
 
 //Midlewares
+const { photoInsertValidation } = require("../middlewares/PhotoValidation");
+const authGuard = require("../middlewares/authGuard");
+const validade = require("../middlewares/handleValidation");
 
 //Routes
 

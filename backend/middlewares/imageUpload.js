19810a732e5f1091c6cb5f0105465bb4b119290f
lucastheldl <lucastheldl@gmail.com>
2023-04-17 12:07:06 -0300

    fixed gitignore

--- backend/middlewares/imageUpload.js
+++ backend/middlewares/imageUpload.js
@@ -3,7 +3,6 @@ const path = require("path");
 const { isUndefined } = require("util");
 
 //destination  to store image
-
 const imageStorage = multer.diskStorage({
   destination: (req, file, cb) => {
     let folder = "";

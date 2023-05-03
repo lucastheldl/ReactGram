367f2047eeda0a931b6aae0eae959b786d772d91
lucastheldl <lucastheldl@gmail.com>
2023-05-03 15:54:27 -0300

    Created form states

--- frontend/src/pages/Auth/Register.tsx
+++ frontend/src/pages/Auth/Register.tsx
@@ -5,22 +5,34 @@ import { Link } from "react-router-dom"
 //hooks
 import { useState,useEffect } from "react"
 import { FormEvent } from "react";
-import { FormEventHandler } from "react";
+
 
 export function Register(){
+  const [name,setName] = useState("");
+  const [email,setEmail] = useState("");
+  const [password,setPassword] = useState("");
+  const [confirmPassword,setConfirmPassword] = useState("");
+  
   function handleSubmit(e: FormEvent<HTMLFormElement>){
     e.preventDefault();
-    
+    const user={
+      name,
+      email,
+      password,
+      confirmPassword
+    }
+
+    console.log(user);
   }
   return(
     <div id="register">
       <h2>ReactGram</h2>
       <p className="subtitle">Cadastre-se para ver as fotos dos seus amigos.</p>
       <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Nome" />
-        <input type="email" placeholder="E-mail" />
-        <input type="password" placeholder="Senha" />
-        <input type="password" placeholder="Confirmar senha" />
+        <input type="text" placeholder="Nome" onChange={(e)=> setName(e.target.value)} value={name || ""}/>
+        <input type="email" placeholder="E-mail" onChange={(e)=> setEmail(e.target.value)} value={email || ""}/>
+        <input type="password" placeholder="Senha" onChange={(e)=> setPassword(e.target.value)} value={password || ""}/>
+        <input type="password" placeholder="Confirmar senha" onChange={(e)=> setConfirmPassword(e.target.value)} value={confirmPassword || ""}/>
         <input type="submit" placeholder="Confirmar" />
       </form>
       <p>
